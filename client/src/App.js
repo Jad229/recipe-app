1d0474e4d79189c099c8cfaccd7650312e726969
Jad229 <jad229@njit.edu>
2023-07-01 18:02:16 -0400

    created router for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-
+import Auth from "./pages/Auth";
+import CreateRecipe from "./pages/CreateRecipe";
+import Home from "./pages/Home";
+import SavedRecipes from "./pages/SavedRecipes";
 function App() {
-  return <div className="App">App Component</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipes" element={<SavedRecipes />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
